49ea05f58ad42b7c5f8c87bb1ec0edcbf95ed66f
Ben <bzho0020@student.monash.edu>
2024-08-14 18:27:11 +1000

    Fix the content block page direction bug
    - add urlhandler controllerr class
    - change the href of brand icon and logout on content block page

---
+++ src/Controller/UrlHandlerController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\View\AppView;
+
+class UrlHandlerController extends AppController
+{
+
+
+    public function global()
+    {
+        return $this->redirect([
+            'plugin' => 'ContentBlocks',
+            'controller' => 'ContentBlocks',
+            'action' => 'index'
+        ]);
+    }
+
+    public function home()
+    {
+        return $this->redirect([
+            'controller' => 'pages',
+            'action' => 'display'
+        ]);
+    }
+
+    public function about_us()
+    {
+        return $this->redirect([
+            'controller' => 'pages',
+            'action' => 'about_us'
+        ]);
+    }
+
+    public function dashboard()
+    {
+        return $this->redirect([
+            'controller' => 'pages',
+            'action' => 'Admin_dashboard'
+        ]);
+    }
+
+    public function logout()
+    {
+        return $this->redirect([
+            'controller' => 'auth',
+            'action' => 'logout'
+        ]);
+    }
+
+
+
+
+}
--- templates/layout/Admin_dashboard.php
+++ templates/layout/Admin_dashboard.php
@@ -19,7 +19,7 @@
 <body class="sb-nav-fixed">
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="School_dashboard.php">Start Bootstrap</a>
+    <a class="navbar-brand ps-3" href="/Urlhandler/dashboard">Start Bootstrap</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
@@ -34,7 +34,7 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="<?= $this->Url->build(['controller'=>'Auth', 'action' => 'logout']) ?>">Logout</a></li>
+                <li><a class="dropdown-item" href="/UrlHandler/logout">Logout</a></li>
             </ul>
         </li>
     </ul>
