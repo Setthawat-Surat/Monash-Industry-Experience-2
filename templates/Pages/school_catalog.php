e1d4bdd48e05934d62dfdb86f1c349e430676b0f
Setthawat <ssat0019@student.monash.edu>
2024-09-07 05:24:00 +1000

    update list in school_catalog

---
+++ templates/Pages/school_catalog.php
@@ -0,0 +1,3 @@
+<?php
+$this->setLayout('frontend');
+?>
--- templates/Pages/support_school.php
+++ templates/Pages/support_school.php
@@ -1,5 +1,8 @@
 <?php
 $this->setLayout('frontend');
+use Cake\ORM\TableRegistry;
+$school_table = TableRegistry::getTableLocator()->get('Schools');
+$school = $school_table->find()->all();
 ?>
 <br><br>
 <section id="search-school-section" class="content-section">
@@ -7,20 +10,19 @@ $this->setLayout('frontend');
         <div class="left-div">
             <h2>Search by school code</h2>
             <br>
-            <form id="search-school-name" class="search-form" action="<?= $this->Url->build(['controller' => 'Search', 'action' => 'results']) ?>" method="get">
+            <form id="search-school-name" class="search-form" action="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'display','school_catalog']) ?>" method="get">
                 <input type="text" name="code" class="search-input" placeholder="Search here...">
                 <button type="submit" class="search-btn">Search</button>
             </form>
         </div>
         <div class="right-div">
             <h2>Search by school name</h2>
-            <form id="search-school-name" class="search-right-form" action="<?= $this->Url->build(['controller' => 'Search', 'action' => 'results']) ?>" method="get">
-                <select class="dropdown" required>
+            <form id="search-school-name" class="search-right-form" action="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'display','school_catalog']) ?>" method="get">
+                <select class="dropdown" name="name" required>
                     <option value="" disabled selected>Select school name</option>
-                    <option value="school1">School 1</option>
-                    <option value="school2">School 2</option>
-                    <option value="school3">School 3</option>
-                    <option value="school4">School 4</option>
+                    <?php foreach ($school as $schools): ?>
+                    <option value="<?=$schools->name?>"><?=$schools->name?></option>
+                    <?php endforeach;?>
                 </select>
                 <button type="submit" class="select-btn">Search</button>
             </form>
