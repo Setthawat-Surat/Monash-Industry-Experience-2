dc40a67de92c9ddf89ad03b1b4069943b0e12d9f
Jadon Fung <jfun0009@student.monash.edu>
2024-09-08 14:15:08 +1000

    Added links to privacy policy and T&Cs to the footer Renamed internal page files to lowercase for compatibility with web server

---
+++ templates/Pages/admin_dashboard.php
@@ -0,0 +1,147 @@
+<?php
+$this->setLayout('Admin_dashboard');
+
+use Cake\ORM\TableRegistry;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <body class="sb-nav-fixed">
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Dashboard</h1><br><br>
+                        <!--
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">Success Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-area me-1"></i>
+                                        Area Chart Example
+                                    </div>
+                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-bar me-1"></i>
+                                        Bar Chart Example
+                                    </div>
+                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                                </div>
+                            </div>
+                        </div>
+                        !-->
+
+                        <?php
+                        $school_table = TableRegistry::getTableLocator()->get('Schools');
+                        $school_rep_account = $school_table->find()->orderBy(['approval_status' => 'ASC'])->toArray();
+
+                        ?>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fa-solid fa-user me-1"></i>
+                                User Accounts
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <?= $this->Flash->render() ?>
+                                    <thead>
+                                        <tr>
+                                            <th>School Name</th>
+                                            <th>School Address</th>
+                                            <th>School Code</th>
+                                            <th>Representative Name</th>
+                                            <th>Representative Email</th>
+                                            <th>Account Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>School Name</th>
+                                            <th>School Address</th>
+                                            <th>School Code</th>
+                                            <th>Representative Name</th>
+                                            <th>Representative Email</th>
+                                            <th>Account Status</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php foreach ($school_rep_account as $school_rep_accounts): ?>
+                                            <tr>
+                                                <td><?=$school_rep_accounts->name?></td>
+                                                <td><?=$school_rep_accounts->address?></td>
+                                                <td><?=$school_rep_accounts->code?></td>
+                                                <td><?=$school_rep_accounts->rep_first_name . ' ' . $school_rep_accounts->rep_last_name?></td>
+                                                <td><?=$school_rep_accounts->rep_email?></td>
+                                                <td>
+                                                    <a href="<?= $this->Url->build(['controller' => 'Schools', 'action' => 'updateAccountStatus', $school_rep_accounts->id, $school_rep_accounts->approval_status]) ?>" onclick="return confirm('Are you sure that you want to change the account status?');">
+                                                        <?= $school_rep_accounts->approval_status == 1 ? 'Verified' : 'Not Verified' ?>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach;?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ templates/Pages/faqs.php
@@ -0,0 +1,95 @@
+<?php
+$this->setLayout('frontend');
+$this->assign('title', 'FAQ');
+?>
+
+
+
+<div class="space"></div>
+
+<!-- ============================================ -->
+<!--                     FAQ                      -->
+<!-- ============================================ -->
+
+<section id="faq-350">
+    <div class="cs-container">
+        <div class="cs-left">
+            <!--Big Image-->
+            <picture class="cs-picture cs-picture1" aria-hidden="true">
+                <source media="(max-width: 600px)" srcset="<?= $this->Url->image('sample_product1.jpg')?>">
+                <source media="(min-width: 601px)" srcset="<?= $this->Url->image('sample_product1.jpg')?>">
+                <img loading="lazy" decoding="async" src="<?= $this->Url->image('sample_product1.jpg')?>" alt="cleaning supplies" width="672" height="672">
+            </picture>
+            <!--Medium Image-->
+            <picture class="cs-picture cs-picture2" aria-hidden="true">
+                <source media="(max-width: 600px)" srcset="<?= $this->Url->image('sample_product2.jpg')?>">
+                <source media="(min-width: 601px)" srcset="<?= $this->Url->image('sample_product2.jpg')?>">
+                <img loading="lazy" decoding="async" src="<?= $this->Url->image('sample_product2.jpg')?>" alt="cleaning supplies" width="300" height="300">
+            </picture>
+            <!--Small Image-->
+            <picture class="cs-picture cs-picture3" aria-hidden="true">
+                <source media="(max-width: 600px)" srcset="<?= $this->Url->image('sample_product3.jpg')?>">
+                <source media="(min-width: 601px)" srcset="<?= $this->Url->image('sample_product3.jpg')?>">
+                <img loading="lazy" decoding="async" src="<?= $this->Url->image('sample_product3.jpg')?>" alt="cleaner" width="200" height="200">
+            </picture>
+        </div>
+        <div class="cs-content">
+            <span class="cs-topper">Asked questions</span>
+            <h2 class="cs-title">Frequently Asked Questions</h2>
+            <ul class="cs-faq-group">
+                <!-- Active class added as default -->
+                <li class="cs-faq-item active">
+                    <button class="cs-button">
+                        <span class="cs-button-text">
+                            <?= $this->ContentBlock->text('faq-question1'); ?>
+                        </span>
+                    </button>
+                    <p class="cs-item-p">
+                        <?= $this->ContentBlock->text('faq-answer1'); ?>
+                    </p>
+                </li>
+                <li class="cs-faq-item">
+                    <button class="cs-button">
+                        <span class="cs-button-text">
+                            <?= $this->ContentBlock->text('faq-question2'); ?>
+                        </span>
+                    </button>
+                    <p class="cs-item-p">
+                        <?= $this->ContentBlock->text('faq-answer2'); ?>
+                    </p>
+                </li>
+                <li class="cs-faq-item">
+                    <button class="cs-button">
+                        <span class="cs-button-text">
+                            <?= $this->ContentBlock->text('faq-question3'); ?>
+                        </span>
+                    </button>
+                    <p class="cs-item-p">
+                        <?= $this->ContentBlock->text('faq-answer3'); ?>
+                    </p>
+                </li>
+                <li class="cs-faq-item">
+                    <button class="cs-button">
+                        <span class="cs-button-text">
+                            <?= $this->ContentBlock->text('faq-question4'); ?>
+                        </span>
+                    </button>
+                    <p class="cs-item-p">
+                        <?= $this->ContentBlock->text('faq-question4'); ?>
+                    </p>
+                </li>
+                <li class="cs-faq-item">
+                    <button class="cs-button">
+                        <span class="cs-button-text">
+                            <?= $this->ContentBlock->text('faq-question5'); ?>
+                        </span>
+                    </button>
+                    <p class="cs-item-p">
+                        <?= $this->ContentBlock->text('faq-answer5'); ?>
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
